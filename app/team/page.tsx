b215ff96fa89baa8befedd764961e6094a7e284b
Shourya <danduuddeepsrishourya2007@gmail.com>
2026-06-09 16:45:32 +0530

    feat: team page updates - wave animation, hover effects, responsive fixes

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import Hero from "@/components/ui/team/Hero";
+
 import Leadership from "@/components/ui/team/Leadership";
 import DomainHeads from "@/components/ui/team/DomainHeads";
 import Departments from "@/components/ui/team/Departments";
-import AllMembersMarquee from "@/components/ui/team/AllMembersMarquee";
 import CursorGlow from "@/components/ui/team/CursorGlow";
 
 export default function TeamPage() {
@@ -15,18 +14,20 @@ export default function TeamPage() {
       `}</style>
 
       <CursorGlow />
-      <Hero />
+     
 
-      <div className="h-px bg-gradient-to-r from-transparent via-white/[0.06] to-transparent mx-6" />
+  
       <Leadership />
 
       <div className="h-px bg-gradient-to-r from-transparent via-white/[0.06] to-transparent mx-6" />
+
       <DomainHeads />
 
       <div className="h-px bg-gradient-to-r from-transparent via-white/[0.06] to-transparent mx-6" />
+
       <Departments />
 
-      <AllMembersMarquee />
+          
     </main>
   );
-}
+} 
